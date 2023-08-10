74f1883ed1289b79938a3617aa5b2dff6bbc77ea
GeeksforGeeksleonidkoltan <leonidkoltan@gmail.com>
2023-08-10 11:33:47 +0500

    Fixed

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,5 @@
-import { PUBLIC_WEB3FORMS_KEY } from '$env/static/public';
+// import { PUBLIC_WEB3FORMS_KEY } from '$env/static/public';
 
-export const config = {
-    web3FormsKey: PUBLIC_WEB3FORMS_KEY
-}
+// export const config = {
+//     web3FormsKey: PUBLIC_WEB3FORMS_KEY
+// }
